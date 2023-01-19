b742166b659649483fc67edad594c74aa448a49c
mporciuncula <122508524+mporciuncula@users.noreply.github.com>
2023-01-20 00:17:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
+import streamlit
+import pandas
+import request
+import snowflake.connector
+from urllib.error import URLError
+
 #import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
