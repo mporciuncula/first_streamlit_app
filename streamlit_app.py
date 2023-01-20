c38c16cf6a67b659acce664b408480d82a2db895
mporciuncula <122508524+mporciuncula@users.noreply.github.com>
2023-01-20 13:37:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruit_to_show)
 
 #Lesson 9 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
 streamlit.write('The user entered ', fruit_choice)
 
 
